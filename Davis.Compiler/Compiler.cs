dff4bd30a22adeb87a1b9f51f5a697a7412fa148
Raven <82341739+bubby932@users.noreply.github.com>
2022-11-01 21:34:21 -0700

    ae

---
+++ Davis.Compiler/Compiler.cs
@@ -0,0 +1,13 @@
+﻿namespace Davis.Compilation
+{
+	using Davis.Parsing;
+	public class Compiler
+	{
+		private readonly List<Token> _Tokens;
+
+		public Compiler(List<Token> tokens)
+		{
+			_Tokens = tokens;
+		}
+	}
+}
