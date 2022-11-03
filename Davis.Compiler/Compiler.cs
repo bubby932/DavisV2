95b4b3f8b8c8203c9f778bb221b5bd861695fca4
Raven <82341739+bubby932@users.noreply.github.com>
2022-11-03 16:00:29 -0700

    working on le thing

--- Davis.Compiler/Compiler.cs
+++ Davis.Compiler/Compiler.cs
@@ -3,11 +3,39 @@
 	using Davis.Parsing;
 	public class Compiler
 	{
-		private readonly List<Token> _Tokens;
+		private readonly Token[] _tokens;
 
-		public Compiler(List<Token> tokens)
+		private int _current = 0;
+
+		public Compiler(Token[] tokens)
 		{
-			_Tokens = tokens;
+			_tokens = tokens;
+		}
+
+
+
+		private bool IsAtEnd() => _current >= _tokens.Length;
+		private Token Advance()
+		{
+			return _tokens[_current++];
+		}
+		private bool Match(TokenType expected)
+		{
+			if (IsAtEnd()) return false;
+			if (_tokens[_current].type != expected) return false;
+
+			_current++;
+			return true;
+		}
+		private Token Peek()
+		{
+			if (IsAtEnd()) return null;
+			return _tokens[_current];
+		}
+		private Token PeekNext()
+		{
+			if (_current + 1 >= _tokens.Length) return null;
+			return _tokens[_current + 1];
 		}
 	}
 }
